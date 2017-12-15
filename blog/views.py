e3aea3b7303d46c546226f95ce14cb543addb108
DenyHs <denilsonh2014@gmail.com>
2017-12-15 13:53:45 -0300

    Template HTML Basic

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
+from django.db.models.functions import Coalesce
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {} )
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by( Coalesce('published_date','title').desc() )
+    return render(request, 'blog/post_list.html', {'posts' : posts} )
