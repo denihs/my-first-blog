c9aef8cd605e0d926d3ed645b5fc6d35ca5daba1
DenyHs <denilsonh2014@gmail.com>
2017-12-16 13:35:23 -0300

    Add new pages

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 from django.db.models.functions import Coalesce
@@ -7,3 +7,8 @@ from django.db.models.functions import Coalesce
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by( Coalesce('published_date','title').desc() )
     return render(request, 'blog/post_list.html', {'posts' : posts} )
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
